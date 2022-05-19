3d3f8fe19bfd2387e15f0ada91f2d9fa0b5318af
Tuba Islam <tubaislam19@gmail.com>
2022-05-20 00:08:31 +0100

    dynamically product add to inventory
    all product info is displayed in placeOrder() function.

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-console.log("I'm In!!!");
-
 let listOfProducts = [
     {
-        item: "A",
-        unitPrice: 50,
+        item: "test",
+        unitPrice: 0,
         multiBuyOffer: true,
-        minQtForSpecialPrice: 3,
-        specialPrice: 130
-    },
-
-    {
-        item: "B",
-        unitPrice: 30,
-        multiBuyOffer: true,
-        minQtForSpecialPrice: 2,
-        specialPrice: 45
-    },
-
-    {
-        item: "C",
-        unitPrice: 20,
-        multiBuyOffer: false
-    },
-
-    {
-        item: "D",
-        unitPrice: 15,
-        multiBuyOffer: false
-    },
+        minQtForSpecialPrice: 0,
+        specialPrice: 0
+    }
 ];
+
+let cartItem = [];
+
+function addProductInStock(item, unitPrice, multiBuyOffer, minQtForSpecialPrice, specialPrice){
+    var tempItem = {};
+    tempItem.item = item;
+    tempItem.unitPrice = unitPrice;
+    tempItem.unitPrice = unitPrice;
+    tempItem.multiBuyOffer = multiBuyOffer;
+    if(multiBuyOffer){
+        tempItem.minQtForSpecialPrice = minQtForSpecialPrice;
+        tempItem.specialPrice = specialPrice;
+    }
+
+    listOfProducts.push(tempItem);
+}
 
 function addToCart(item){
 
@@ -41,15 +35,23 @@ function checkout(){
 function placeOrder() {
     console.log("Your order is successfully placed.");
     console.log("= = = = = = Order Summary = = = = = =");
+    // for the time being displaying all products info
     for (i in listOfProducts){
 
-        // console.log(Object.keys(listOfProducts[i]));
+        // Displaying Product Info
         let tempItem = listOfProducts[i];
         for (j in Object.keys(tempItem)){
             console.log(Object.keys(tempItem)[j] + ": " + Object.values(tempItem)[j]);
         }
+        console.log("------------------------");
     }
     console.log("= = = = = = = = = = = = = = = = = = =");
 }
+
+addProductInStock("A", 50, true, 3, 130);
+addProductInStock("B", 30, true, 2, 45);
+addProductInStock("C", 20, false);
+addProductInStock("D", 15, false);
+addProductInStock("E", 10, true, 5, 40);
 
 placeOrder();
